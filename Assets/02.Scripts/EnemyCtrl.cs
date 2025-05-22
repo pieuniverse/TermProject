92d555e616ec40533dea36b3fffb33cfce1c7389
WOO JI WON <149660416+pieuniverse@users.noreply.github.com>
2025-05-23 00:03:49 +0900

    플레이어 이동&회전 구현 / 포인트 y축 좌표 조정 필요

--- Assets/02.Scripts/EnemyCtrl.cs
+++ Assets/02.Scripts/EnemyCtrl.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class AnemyCtrl : MonoBehaviour
+public class EnemyCtrl : MonoBehaviour
 {
     public Transform pointA;
     public Transform pointB;
@@ -22,7 +22,7 @@ public class AnemyCtrl : MonoBehaviour
         if (isTurning) return; // 회전 중에는 이동하지 않음
 
         // 이동 애니메이션 재생
-        animator.Play("Walking");
+        animator.Play("RunAim");
 
         // 이동
         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
@@ -30,9 +30,10 @@ public class AnemyCtrl : MonoBehaviour
         // 반환점 도착 처리
         if (Vector3.Distance(transform.position, target) < 0.1f)
         {
-            StartCoroutine(PlayTurnAnimation());
+            //StartCoroutine(PlayTurnAnimation());
             // 다음 목표 지점 변경
             target = (target == pointA.position) ? pointB.position : pointA.position;
+            transform.Rotate(0f, 180f, 0f);
         }
     }
 
@@ -40,7 +41,7 @@ public class AnemyCtrl : MonoBehaviour
     private System.Collections.IEnumerator PlayTurnAnimation()
     {
         isTurning = true;
-        animator.SetTrigger("Turn"); // 상태머신에서 Turn 트리거 활성화
+        //animator.SetTrigger("Turn"); // 상태머신에서 Turn 트리거 활성화
 
         // 애니메이션 B가 끝날 때까지 대기 (길이에 따라 수정)
         yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
